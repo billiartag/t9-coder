0b0ff52df1d734c732a41882a053d93dda4c3c17
Cosmas Yustianus Billiarta Gondokusuma <cosmas.yustianus@sby.spil>
2025-07-16 14:27:50 +0700

    add reverse & delete function

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,7 @@ const numbers = [
 ];
 
 const combinations = {
+  ' ': '0',
   a: "2",
   b: "22",
   c: "222",
@@ -87,39 +88,76 @@ const combinations = {
 };
 
 ///
-
-function addNumber(title){
-  console.log(title);
-  textData.value = textData.value + title;
-  console.log(textData);
+function resetField() {
+  textData.value = "";
 }
 
-function reverseText(){
-  console.log('current '+textData.value);
+function addNumber(title) {
+  // check for number or string
+  if (parseFloat(textData.value) || textData.value == "") { // number
+    console.log((title == '1' || title == '*' || title == '#'));
+    if (title == '1' || title == '*' || title == '#') { return; }
+    textData.value = textData.value + title;
+  } else { // string
+
+  }
+}
+
+function reverseText() {
+  var translated = "";
+
+  if (parseFloat(textData.value)) {
+    const groups = textData.value.match(/(\d)\1*/g);
+    // swap combination first
+    const swapped = {};
+    for (const [key, value] of Object.entries(combinations)) {
+      swapped[value] = key;
+    }
+    // match 
+    groups.forEach(element => {
+      if (element == "0") {
+        translated += " ";
+      }
+      else {
+        translated += swapped[element];
+      }
+    });
+
+  } else {
+    const groups = textData.value.split("");
+    // match
+    groups.forEach(element => {
+      if (element == " ") {
+        translated += "0"
+      } else {
+        translated += combinations[element] + " ";
+      }
+    });
+  }
+  textData.value = translated;
 }
 
 </script>
 
 <template>
   <main class="container">
-    <div class="mb-5"> 
+    <div class="mb-5">
       <input type="text" class="form-control" v-model="textData">
       <div class="m-2 d-flex justify-content-center">
         <button class="btn btn-primary mx-1">Process</button>
         <button class="btn btn-outline-primary mx-1" @click="reverseText">Reverse</button>
-        <button class="btn btn-outline-primary mx-1"> 
+        <button class="btn btn-outline-primary mx-1" @click="resetField">
           <i class="fa-solid fa-delete-left"></i>
         </button>
-        <button class="btn btn-outline-primary mx-1"> 
+        <button class="btn btn-outline-primary mx-1">
           <i class="fa-solid fa-copy"></i>
         </button>
       </div>
     </div>
     <div class=" row d-flex justify-content-center">
-      <t-button v-for="item in numbers" :title="item.title" :subtitle="item.subtitle" @click="addNumber(item.title)"/>
+      <t-button v-for="item in numbers" :title="item.title" :subtitle="item.subtitle" @click="addNumber(item.title)" />
     </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
